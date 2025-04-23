f97fe73094b1b0db9376e6570323c469d4a05c4c
Yen Ho <yenhn@devblock.net>
2025-04-23 10:26:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function sayHello() {
   alert("Chào mừng bạn đến với website đầu tiên!");
 }
+
+var ten = document.getElementById("name").value;
+var tuoi = document.getElementById("year").value;
+var gioiTinh = document.getElementById("gioiTinh").value;
+var gioiTinhCal = gioiTinh == "nam"?"Nam": "Nữ";
+
+function Tinh(
+  alert("Tên: "+ ten + ", Tuổi: " + tuoi + ", Giới tính: " + gioiTinhCal);
+)
+
+function tuoi(){
+  
+}
+
+function year(){
+  
+}
+
+function sao(){
+  
+}
+
+function han(){
+  
+}
